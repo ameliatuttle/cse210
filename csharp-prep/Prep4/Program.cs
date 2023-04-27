1ef37619b16fd8a80445908d3f1599a12be79943
ameliatuttle <aetuttle@byui.edu>
2023-04-26 20:42:08 -0600

    prep 4 doneeee

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number = -1;
+        List<int> numbers = new List<int>();
+
+        while (number != 0)
+        {
+            Console.Write("Enter Number: ");
+            number = int.Parse(Console.ReadLine());
+
+            numbers.Add(number);
+        }
+
+        int sum = numbers.Sum();
+        float average = sum / (numbers.Count - 1);
+        int max = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
