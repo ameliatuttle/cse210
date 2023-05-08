1489d8b683239fe34cbb3abadfd0e51483b4ee49
ameliatuttle <aetuttle@byui.edu>
2023-05-08 11:08:44 -0600

    complete journal program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,29 @@
 using System;
 
-class Program
+namespace Develop02 
 {
-    static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello Develop02 World!");
+        static void Main(string[] args)
+        {
+            // add some test entries
+            Entry myEntry = new Entry();
+            myEntry.StorePrompt("What is your name?");
+            myEntry.StoreResponse("Mathew Manley");
+            myEntry.StoreDate("8 May 2023");
+
+            // create a journal to test
+            Journal journal = new Journal();
+            journal.StoreEntry(myEntry);
+
+            // show the journal contents
+            List<Entry> entries = journal.GetEntries();
+            foreach (Entry entry in entries)
+            {
+                string message
+                    = $"{entry.GetDate()}\n{entry.GetPrompt()}\n{entry.GetResponse()}\n";
+                Console.WriteLine(message);
+            }
+        }
     }
 }
